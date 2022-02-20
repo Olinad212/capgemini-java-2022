39e5183d172cc74eb3c5ca8e1be6e70f082c1ba9
Danilo Nogueira <danilonogsi@gmail.com>
2022-02-20 16:38:26 -0300

    Acréscimo de documentação e alteracao do caractere pipe para espaco

--- academia-capgemini-2022/src/br/com/proway/capgemini/Questao01.java
+++ academia-capgemini-2022/src/br/com/proway/capgemini/Questao01.java
@@ -3,18 +3,24 @@ package br.com.proway.capgemini;
 import java.util.Scanner;
 
 public class Questao01 {
+	
+	/**
+	 * @author Danilo Nogueira da Silva
+	 * Esta classe imprime uma escada com o tamanho/quantidade de niveis informado pelo usuario.
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
 		int altura, degraus, espaco;
 		
-		System.out.println("Informe o tamanho da escada: ");
+		System.out.print("Informe o tamanho da escada: ");
 		Integer tamanhoDaEscada = entrada.nextInt();
 		
 		for(altura = 1; altura <= tamanhoDaEscada; altura++) {
 			for(espaco = tamanhoDaEscada; espaco > altura; espaco--) {
-				System.out.print("|");
+				System.out.print(" ");
 			}
 			for(degraus = 1; degraus <= altura; degraus++) {
 				System.out.print("*");
